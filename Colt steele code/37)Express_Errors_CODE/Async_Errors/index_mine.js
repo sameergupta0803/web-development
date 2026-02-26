cf4c86c02c33c74899b558161582cf3f9cb8d6a8
Sameer Gupta <sameergupta0803@gmail.com>
2026-02-27 02:59:38 +0530

    completed error handlng with express

--- Colt steele code/37)Express_Errors_CODE/Async_Errors/index_mine.js
+++ Colt steele code/37)Express_Errors_CODE/Async_Errors/index_mine.js
@@ -3,10 +3,14 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
-
+const AppError = require('./AppError');
 
 const Product = require('./models/product');
-
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e))
+    }
+}
 mongoose.connect('mongodb://localhost:27017/farmStand2', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
@@ -25,7 +29,7 @@ app.use(methodOverride('_method'))
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
-app.get('/products', async (req, res) => {
+app.get('/products', wrapAsync(async (req, res, next) => {
     const { category } = req.query;
     if (category) {
         const products = await Product.find({ category })
@@ -34,43 +38,76 @@ app.get('/products', async (req, res) => {
         const products = await Product.find({})
         res.render('products/index', { products, category: 'All' })
     }
-})
+}))
 
 app.get('/products/new', (req, res) => {
     res.render('products/new', { categories })
 })
-
-app.post('/products', async (req, res) => {
+app.post('/products', wrapAsync(async (req, res, next) => {
     const newProduct = new Product(req.body);
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`)
-})
+}))
+// app.post('/products', async (req, res, next) => {
+//     try {
+//         const newProduct = new Product(req.body);
+//         await newProduct.save();
+//         res.redirect(`/products/${newProduct._id}`)
+//     }
+//     catch (e) {
+//         next(e);
+//     }
+//     //we use try and catch to catch all the errors an async function can throw,and then we pass it using next to our error handler
+// })
 
-app.get('/products/:id', async (req, res) => {
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findById(id)
+    if (!product) {
+        throw new AppError("product not found", 404)
+        // return next(new AppError("product not found", 404))
+        //we use next to pass error to out error handler in async functions
+    }
     res.render('products/show', { product })
-})
+}))
 
-app.get('/products/:id/edit', async (req, res) => {
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findById(id);
+    if (!product) {
+        return next(new AppError("product not found", 404))
+    }
     res.render('products/edit', { product, categories })
-})
 
-app.put('/products/:id', async (req, res) => {
+}))
+
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
     res.redirect(`/products/${product._id}`);
-})
+}))
 
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
     res.redirect('/products');
+}))
+const handleValidationErr = (err) => {
+    // console.dir(err);
+    return new AppError(`Validation failed... ${err.message}`, 400)
+    //specific message and status for validation error
+}
+app.use((err, req, res, next) => {
+    const name = err.name;
+    if (name == 'ValidationError') err = handleValidationErr(err);
+    //Handling specific types of mongoose error with err.name property
+    next(err);
 })
-
-
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Something went wrong" } = err;
+    console.log(err.name);
+    res.status(status).send(message);
+})
 
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!")
